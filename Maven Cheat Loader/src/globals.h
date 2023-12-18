f6787b7d117207b64a3f0fa607f5e47c61e9b8ef
MavenCoding157 <117538886+MavenCoding157@users.noreply.github.com>
2023-12-18 23:54:02 +0000

    Add files via upload

---
+++ Maven Cheat Loader/src/globals.h
@@ -0,0 +1,65 @@
+#pragma once
+//some of these includes are useless but cba to remove them
+#include <cstdint>
+#include <cstddef>
+#include <ctime>
+#include "antiAC.h"
+#include "../imgui/imgui.h"
+#include "../imgui/imgui_impl_dx9.h"
+#include "../imgui/imgui_impl_win32.h"
+#include "../imgui/imgui_stdlib.h"
+#include <string>
+#include <sstream>
+#include <fstream>
+#include <vector>
+#include <tchar.h>
+#include <tlhelp32.h>
+#include <WtsApi32.h>
+#pragma comment(lib, "Wtsapi32.lib")
+#include <iostream>
+#include <windows.h>
+#include <thread>
+#include <d3d9.h>
+
+
+
+namespace globals
+{
+	inline float Backgroundcolor[] = { 1.f, 0.f, 0.f, 1.f };
+}
+
+
+namespace checkboxes
+{
+	
+}
+
+//for injection status
+inline bool Injected = false;
+
+
+//for hwid spoofer
+
+//for spoofer
+static void SpooferComingSoon() {
+	MessageBox(NULL, "Coming Soon...", "Maven Cheat Loader", MB_ICONINFORMATION | MB_OK);
+}
+
+static void spoofer()
+{
+	SpooferComingSoon();
+}
+//to here
+
+//for cleaner
+static void CleanerComingSoon() {
+	MessageBox(NULL, "Coming Soon...", "Maven Cheat Loader", MB_ICONINFORMATION | MB_OK);
+}
+
+static void cleaner()
+{
+	CleanerComingSoon();
+}
+//to here
+
+//to here
---
+++ Maven Cheat Loader/src/main.cpp
@@ -0,0 +1,61 @@
+#include "gui.h"
+#include "globals.h"
+#include <thread>
+#include <iostream>
+#include "antiAC.h"
+
+
+//popup
+void StartMessageBox() {
+	MessageBox(NULL, "Loading Loader Bypass, Please Wait....", "Maven Cheat Loader", MB_ICONINFORMATION | MB_OK); //doesnt mean anything (YET) just to make it look cool
+}
+
+//code for generating the random string exe name
+std::string generateRandomString(int length) {
+	std::string randomString;
+	static const char alphanum[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
+
+	srand(static_cast<unsigned int>(time(nullptr)));
+
+	for (int i = 0; i < length; ++i) {
+		randomString += alphanum[rand() % (sizeof(alphanum) - 1)];
+	}
+
+	return randomString;
+}
+//to here
+
+int __stdcall wWinMain(
+	HINSTANCE instance,
+	HINSTANCE previousInstance,
+	PWSTR arguments,
+	int commandShow)
+
+{
+
+	StartMessageBox();// Display the message box when the application is run
+	std::this_thread::sleep_for(std::chrono::seconds(3));// makes program sleep for 3 seconds
+
+
+	// create gui
+	std::string randomString = generateRandomString(12); //generates the random string
+	gui::CreateHWindow(randomString.c_str()); //assign the random string name to the application
+	gui::CreateDevice();
+	gui::CreateImGui();
+
+	while (gui::isRunning)
+	{
+		gui::BeginRender();
+		gui::Render();
+		gui::EndRender();
+
+		std::this_thread::sleep_for(std::chrono::milliseconds(5));
+	}
+
+	// destroy gui
+	gui::DestroyImGui();
+	gui::DestroyDevice();
+	gui::DestroyHWindow();
+
+	return EXIT_SUCCESS;
+}
